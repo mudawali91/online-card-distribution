3c7a0e2172881787d6b445616493baf897c40ed3
Muda Wali Bin Abdullah <mudawali@cloone.com.my>
2023-05-21 18:32:39 +0800

    Create index as main page

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="Muda Wali - Tyrell System Programing Test">
+	<title>Tyrell System Programing Test | Q1</title>
+
+	<!-- Begin CSS -->
+	<link href="public/assets/css/bootstrap.min.css" rel="stylesheet">
+	<!-- End CSS -->
+</head>
+<body>
+	
+	<div class="container">
+
+		<h3>A) Programming Test</h3>
+		<div class="card">
+			<div class="card-header">
+				Theme: Playing cards will be given out to n(number) people
+			</div>
+			<div class="card-body">
+				<h6 class="card-title">Input Instruction:</h6>
+				<p class="card-text">
+					<ol type="a">
+						<li>Number of people (numerical value)</li>
+						<li>It does not matter how cards are given if recompile of program arguments, parameter, keyboard input and so on are not necessary.</li>
+						<li>In case input value is nil or value is invalid then the error message of "Input value does not exist or value is invalid" must be displayed and the process must be terminated.</li>
+						<li>Any number less than 0 is an invalid value.</li>
+						<li>Greater than 53 are normal values and cards must be distributed to a number of people instead of having it as an error.</li>
+					</ol>
+				</p>
+			</div>
+		</div>
+
+	</div>
+
+	<!-- Begin JS -->
+	<script src="public/assets/js/vendor/jquery.min.js"></script>
+	<script src="public/assets/js/bootstrap.bundle.min.js"></script>
+	<!-- End JS -->
+</body>
+</html>
