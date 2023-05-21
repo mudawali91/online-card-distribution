54f3b1b9dcd0dbf424192cc42d8d00ae02c2b342
Muda Wali Bin Abdullah <mudawali@cloone.com.my>
2023-05-21 22:43:09 +0800

    Create system configuration file and include at main page

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+	header('Content-Type: text/html; charset=UTF-8');
+
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
+	date_default_timezone_set("Asia/Kuala_Lumpur");
+?>
