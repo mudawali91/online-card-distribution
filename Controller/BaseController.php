65f8ac4e40fe36eff6344452494fdb01f511d7e4
Muda Wali Bin Abdullah <mudawali@cloone.com.my>
2023-05-21 23:12:30 +0800

    Validate form request from server-side

--- Controller/BaseController.php
+++ Controller/BaseController.php
@@ -11,6 +11,24 @@
 			$no_of_player = ( isset($_POST['no_of_player']) ? $_POST['no_of_player'] : 0 );
 			$data['no_of_player'] = $no_of_player;
 
+			// start validate input request
+			$error_msg = '';
+
+			if ( empty($no_of_player) || filter_var($no_of_player, FILTER_VALIDATE_INT) === false || $no_of_player <= 0 ) {
+				$error_msg = 'Input value does not exist or value is invalid';
+			}
+
+			if ( !empty($error_msg) ) {
+				$response = [
+					'status' => $status,
+					'message' => $error_msg,
+					'data' => $data,
+				];
+				print_json($response, 200);
+				return false;
+			}
+			// end validate input request
+
 			$response = [
 				'status' => 1,
 				'message' => 'Success',
