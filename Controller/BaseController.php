4de7bf449a17efe9eb0fa412f2b5c8d5b9dca001
Muda Wali Bin Abdullah <mudawali@cloone.com.my>
2023-05-21 23:42:00 +0800

    Randomly shuffle cards

--- Controller/BaseController.php
+++ Controller/BaseController.php
@@ -34,10 +34,12 @@
 			// initialize card object
 			$card = new CardController();
 
-			// $data['card_type'] = $card->get_card_type();
-			// $data['card_list'] = $card->get_card_list();
 			$data['card_arrangement'] = $card_arrangement = $card->get_card_arrangement();
 			$data['total_card'] = $total_card = count($card_arrangement);
+
+			// randomly shuffle cards
+			shuffle($card_arrangement);
+			$data['card_arrangement_shuffle'] = $card_arrangement;
 
 			$response = [
 				'status' => 1,
